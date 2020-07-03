271c240585eaf7b5f6f20794897017711fccf2ac
Steve Williamson <steve@sjdub.com>
2020-07-03 14:23:29 -0400

    stripe creds in cashier config now

--- src/CashierToolController.php
+++ src/CashierToolController.php
@@ -37,9 +37,9 @@ class CashierToolController extends Controller
     public function __construct(Repository $config)
     {
         $this->middleware(function ($request, $next) use ($config) {
-            Stripe::setApiKey($config->get('services.stripe.secret'));
+            Stripe::setApiKey($config->get('cashier.secret'));
 
-            $this->stripeModel = $config->get('services.stripe.model');
+            $this->stripeModel = $config->get('cashier.model');
 
             $this->subscriptionName = $config->get('nova-cashier-manager.subscription_name');
 
